63d8231cb8a8fa5119dcfe920b8ea52235b122e1
youngbamer <0421mk@naver.com>
2021-10-16 16:44:01 +0900

    while로 팩토리얼 구현 문제

--- Project1/HelloWorld.c
+++ Project1/HelloWorld.c
@@ -2,23 +2,38 @@
 
 int main()
 {
-	int i = 0;
-	int input = 0;
+	//문제1 : 두 수를 입력받아서 첫 수부터 두번째 수까지의 합
+	/*
+	int input1 = 0;
+	int input2 = 0;
 	int total = 0;
-	while (i < 5) {
 
-		scanf_s("%d", &input);
+	scanf_s("%d", &input1);
+	scanf_s("%d", &input2);
 
-		if (input < 1) {
-			printf("1 이상의 수를 입력해주세요.\n");
-			continue;
-		}
-
-		total += input;
-		i++;
+	while (input1 <= input2) {
+		total += input1;
+		input1++;
 	}
 
 	printf("%d", total);
+
+	return 0;
+	*/
+	
+	// 팩토리얼 구현
+	int input = 0;
+	int result = 1;
+	int i = 1;
+
+	scanf_s("%d", &input);
+
+	while (i <= input) {
+		result *= i;
+		i++;
+	}
+
+	printf("%d", result);
 
 	return 0;
 }
