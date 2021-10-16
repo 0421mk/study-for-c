9405f704c1c051494446b8ab73be0331c2ba0062
youngbamer <0421mk@naver.com>
2021-10-16 23:21:23 +0900

    시, 분, 초 출력 프로그램

--- Project1/HelloWorld.c
+++ Project1/HelloWorld.c
@@ -1,34 +1,15 @@
 #include <stdio.h>
-int primeNumber(int i);
+
 // 모든 경우의 수를 다 확인합니다.
 int main() {
-	int count = 0;
-	int i = 1;
-	while(count != 10) {
-		
-		if (primeNumber(i) == 1) {
-			printf("%d ", i);
-			count++;
-		}
+	int input;
+	scanf_s("%d", &input);
 
-		i++;
-	}
+	int hour = input / 3600;
+	int minute = input % 3600 / 60;
+	int second = (input % 3600) % 60;
 
-	return 0;
-}
-
-int primeNumber(int i) {
-	int j = 1;
-	int count = 0;
-	for (int j = 1; j <= i; j++) {
-		if (i % j == 0) {
-			count++;
-		}
-	}
-
-	if (count == 2) {
-		return 1;
-	}
-
+	printf("h:%d, m:%d, s:%d", hour, minute, second);
+	
 	return 0;
 }
