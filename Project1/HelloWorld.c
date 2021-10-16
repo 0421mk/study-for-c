b7866e95be9b4da6eeb3cb80fd3c9f3be7d18023
youngbamer <0421mk@naver.com>
2021-10-16 23:13:31 +0900

    소수 10개 출력하기 문제

--- Project1/HelloWorld.c
+++ Project1/HelloWorld.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
-
+int primeNumber(int i);
 // 모든 경우의 수를 다 확인합니다.
 int main() {
-	int cream;
-	int shrimp;
-	int cola;
-
-	int price = 3500;
-
-	for (cream = 500; cream < price; cream += 500) {
-		printf("크림빵 : %d\n", cream);
-		for (shrimp = 700; shrimp < price; shrimp += 700) {
-			printf("새우 : %d\n", shrimp);
-			for (cola = 400; cola < price; cola += 400) {
-				printf("콜라 : %d\n", cola);
-				if (cream + shrimp + cola == price) {
-					printf("크림빵 %d, 새우깡 %d, 콜라 %d개", cream / 500, shrimp / 700, cola / 400);
-				}
-				else {
-					printf("금액이 맞지 않습니다.");
-					continue;
-				}
-			}
+	int count = 0;
+	int i = 1;
+	while(count != 10) {
+		
+		if (primeNumber(i) == 1) {
+			printf("%d ", i);
+			count++;
 		}
+
+		i++;
+	}
+
+	return 0;
+}
+
+int primeNumber(int i) {
+	int j = 1;
+	int count = 0;
+	for (int j = 1; j <= i; j++) {
+		if (i % j == 0) {
+			count++;
+		}
+	}
+
+	if (count == 2) {
+		return 1;
 	}
 
 	return 0;
