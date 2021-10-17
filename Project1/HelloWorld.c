1722e3f5540c0e18ac6d55ab4947676c6dd10946
youngbamer <0421mk@naver.com>
2021-10-17 13:54:28 +0900

    포인터를 이용한 배열의 접근

--- Project1/HelloWorld.c
+++ Project1/HelloWorld.c
@@ -1,26 +1,81 @@
 #include <stdio.h>
 
+// 포인터 테스트 문제
+/*
 int main() {
 	
-	int num1 = 10;
-	int num2 = 20;
+	int arr[5] = { 1,2,3,4,5 };
 
-	// 주소값을 선언하고 num1, num2의 메모리 주소값을 가르킴
-	int* ptr1 = &num1;
-	int* ptr2 = &num2;
-	int* temp;
+	int* ptr = arr;
+	for (int i = 0; i < 5; i++) {
+		*ptr += 2;
+		// ptr은 주소값을 갖는 임시변수로 이용
+		ptr++;
+	}
 
-	// 직접 주소로 찾아가 값을 참조
-	(*ptr1) += 10; // num1 = 20
-	(*ptr2) -= 10; // num2 = 10
+	for (int i = 0; i < 5; i++) {
+		printf("%d\n", arr[i]);
+	}
 
-	// 주소값을 서로 변경함
-	temp = ptr1;
-	ptr1 = ptr2;
-	ptr2 = temp;
+	return 0;
+}
+*/
+
+/*
+int main() {
+	int arr[5] = { 1,2,3,4,5 };
+
+	int* ptr = arr;
+	for (int i = 0; i < 5; i++) {
+		// 실제 주소로 접근하는 것이므로 값이 바뀝니다.
+		*(ptr + i) += 2;
+	}
+
+	for (int i = 0; i < 5; i++) {
+		printf("%d\n", arr[i]);
+	}
+
+	return 0;
+}
+*/
+
+/*
+int main() {
+	int arr[5] = { 1,2,3,4,5 };
+	int* ptr = arr + 4;
+	int total = 0;
+
+	for (int i = 0; i < 5; i++) {
+		total += *ptr;
+		ptr--;
+	}
+
+	printf("%d", total);
+
+	return 0;
+}
+*/
+
+// 포인터를 활용하여 역순으로 정렬
+int main() {
+	int arr[6] = { 1,2,3,4,5,6 };
+
+	int* ptr1 = &arr[0];
+	int* ptr2 = &arr[5];
+	int temp;
+
+	for (int i = 0; i < 3; i++) {
+		temp = *ptr1;
+		*ptr1 = *ptr2;
+		*ptr2 = temp;
+
+		ptr1++;
+		ptr2--;
+	}
+
+	for (int i = 0; i < 6; i++) {
+		printf("%d ", arr[i]);
+	}
 	
-	// 직접 주소로 찾아가 값을 참조
-	printf("%d, %d\n", *ptr1, *ptr2);
-
 	return 0;
 }
