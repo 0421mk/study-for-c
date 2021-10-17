daae06119707322fe188fcf62d06ba36a1cb7642
youngbamer <0421mk@naver.com>
2021-10-17 23:30:06 +0900

    최댓값, 최솟값, 정수의 합

--- Project1/HelloWorld.c
+++ Project1/HelloWorld.c
@@ -1,81 +1,34 @@
 #include <stdio.h>
 
-// 포인터 테스트 문제
-/*
+// 정수의 합, 최댓값, 최솟값 배열로 구하기
 int main() {
-	
-	int arr[5] = { 1,2,3,4,5 };
-
-	int* ptr = arr;
-	for (int i = 0; i < 5; i++) {
-		*ptr += 2;
-		// ptr은 주소값을 갖는 임시변수로 이용
-		ptr++;
-	}
-
-	for (int i = 0; i < 5; i++) {
-		printf("%d\n", arr[i]);
-	}
-
-	return 0;
-}
-*/
-
-/*
-int main() {
-	int arr[5] = { 1,2,3,4,5 };
-
-	int* ptr = arr;
-	for (int i = 0; i < 5; i++) {
-		// 실제 주소로 접근하는 것이므로 값이 바뀝니다.
-		*(ptr + i) += 2;
-	}
-
-	for (int i = 0; i < 5; i++) {
-		printf("%d\n", arr[i]);
-	}
-
-	return 0;
-}
-*/
-
-/*
-int main() {
-	int arr[5] = { 1,2,3,4,5 };
-	int* ptr = arr + 4;
+	int arr[5];
+	int input;
+	int max = 0;
+	int min = 0;
 	int total = 0;
 
 	for (int i = 0; i < 5; i++) {
-		total += *ptr;
-		ptr--;
+		scanf_s("%d", &input);
+		arr[i] = input;
+
+		if (max < arr[i]) {
+			max = arr[i];
+		}
+
+		if (i == 0) {
+			min = arr[i];
+		}
+		else {
+			if (min > arr[i]) {
+				min = arr[i];
+			}
+		}
+		
+		total += arr[i];
 	}
 
-	printf("%d", total);
+	printf("최댓값 : %d, 최솟값 : %d, 정수의 합 : %d", max, min, total);
 
-	return 0;
-}
-*/
-
-// 포인터를 활용하여 역순으로 정렬
-int main() {
-	int arr[6] = { 1,2,3,4,5,6 };
-
-	int* ptr1 = &arr[0];
-	int* ptr2 = &arr[5];
-	int temp;
-
-	for (int i = 0; i < 3; i++) {
-		temp = *ptr1;
-		*ptr1 = *ptr2;
-		*ptr2 = temp;
-
-		ptr1++;
-		ptr2--;
-	}
-
-	for (int i = 0; i < 6; i++) {
-		printf("%d ", arr[i]);
-	}
-	
 	return 0;
 }
