db4b73734b8cfcbb272c3ff79a0f50ffe1741acb
youngbamer <0421mk@naver.com>
2021-10-17 13:05:36 +0900

    포인터의 이해

--- Project1/HelloWorld.c
+++ Project1/HelloWorld.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
-int recursive(int n);
-// 모든 경우의 수를 다 확인합니다.
+
 int main() {
-	int input;
-
-	scanf_s("%d", &input);
-
-	printf("%d", recursive(input));
 	
+	int num1 = 10;
+	int num2 = 20;
+
+	// 주소값을 선언하고 num1, num2의 메모리 주소값을 가르킴
+	int* ptr1 = &num1;
+	int* ptr2 = &num2;
+	int* temp;
+
+	// 직접 주소로 찾아가 값을 참조
+	(*ptr1) += 10; // num1 = 20
+	(*ptr2) -= 10; // num2 = 10
+
+	// 주소값을 서로 변경함
+	temp = ptr1;
+	ptr1 = ptr2;
+	ptr2 = temp;
+	
+	// 직접 주소로 찾아가 값을 참조
+	printf("%d, %d\n", *ptr1, *ptr2);
+
 	return 0;
-}
-
-int recursive(int n) {
-
-	if (n == 1) {
-		return 1;
-	}
-
-	return n * recursive(n-1);
 }
