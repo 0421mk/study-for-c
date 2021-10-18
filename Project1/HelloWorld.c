ec66664848d7765fbf2d1cd15294d3309360fc36
youngbamer <0421mk@naver.com>
2021-10-18 13:41:00 +0900

    배열 홀수, 짝수 거꾸로 출력 문제

--- Project1/HelloWorld.c
+++ Project1/HelloWorld.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-// 홀수, 짝수 출력 
-/*
+// 배열에서 홀수면 앞, 짝수면 거꾸로 뒤로 출력
 int main() {
 	int input;
 	int arr1[10];
 	int arr2[10];
+	int result[10];
 	int j = 0;
 	int k = 0;
 	int arrLen = 0;
+	int r = 0;
 
 	for (int i = 0; i < 10; i++) {
 		scanf_s("%d", &input);
-		
+
 		if (input % 2 == 1) {
 			arr1[j] = input;
 			j++;
@@ -22,59 +23,26 @@ int main() {
 			k++;
 		}
 	}
-	
-	printf("홀수 출력 : ");
+
 	while (arrLen < j) {
-		if (arrLen == j - 1) {
-			printf("%d", arr1[arrLen]);
-		}
-		else {
-			printf("%d, ", arr1[arrLen]);
-		}
+		result[r] = arr1[arrLen];
 		arrLen++;
+		r++;
 	}
 
 	printf("\n");
-	arrLen = 0;
+	arrLen = k - 1;
 
-	printf("짝수 출력 : ");
-	while (arrLen < k) {
-		if (arrLen == k - 1) {
-			printf("%d", arr2[arrLen]);
-		}
-		else {
-			printf("%d, ", arr2[arrLen]);
-		}
-		arrLen++;
+	while (arrLen >= 0) {
+		result[r] = arr2[arrLen];
+		r++;
+		arrLen--;
 	}
 
-	return 0;
-}
-*/
+	printf("배열 요소의 출력 : ");
 
-// 10진수 형태로 입력받아서 2진수로 출력
-int main() {
-	int input = 0;
-	int arr[100];
-	int i = 0;
-	scanf_s("%d", &input);
-
-	while (input != 0) {
-		if (input % 2 == 0) {
-			input /= 2;
-			arr[i] = 0;
-		}
-		else if(input % 2 == 1) {
-			input -= 1;
-			input /= 2;
-			arr[i] = 1;
-		}
-
-		i++;
-	}
-
-	for (int j = i - 1; j >= 0; j--) {
-		printf("%d", arr[j]);
+	for (int i = 0; i < r; i++) {
+		printf("%d ", result[i]);
 	}
 
 	return 0;
