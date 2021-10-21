02c0814893bcdbd7d587a5f40cd023529af8c323
youngbamer <0421mk@naver.com>
2021-10-21 14:33:00 +0900

    구조체를 이용하여 직사각형의 면적과 좌표 출력

--- Project1/HelloWorld.c
+++ Project1/HelloWorld.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// 구조체 변수의 연산 swapPoint 함수 구현
+// 구조체를 이용하여 직사각형의 넓이와 좌표 출력
 typedef struct point
 {
 	int xpos;
 	int ypos;
 } Point;
 
-void swapPoint(Point* pos1, Point* pos2) {
-	int temp = pos1 -> xpos;
-	pos1 -> xpos = pos2 -> xpos;
-	pos2 -> xpos = temp;
+int room(Point pos1, Point pos2) {
+	int height;
+	int width;
 
-	temp = pos1 -> ypos;
-	pos1 -> ypos = pos2 -> ypos;
-	pos2 -> ypos = temp;
+	width = abs(pos1.xpos - pos2.xpos);
+	height = abs(pos1.ypos - pos2.ypos);
+
+	return width * height;
 }
 
 int main() {
-	Point pos1 = { 2,4 };
-	Point pos2 = { 5,7 };
+	Point pos1 = { 0,0 };
+	Point pos2 = { 100,100 };
 
-	swapPoint(&pos1, &pos2);
+	int roomVal = room(pos1, pos2);
 
+	printf("%d\n", roomVal);
 	printf("%d %d\n", pos1.xpos, pos1.ypos);
+	printf("%d %d\n", pos1.xpos, pos2.ypos);
+	printf("%d %d\n", pos2.xpos, pos1.ypos);
 	printf("%d %d\n", pos2.xpos, pos2.ypos);
 
 	return 0;
