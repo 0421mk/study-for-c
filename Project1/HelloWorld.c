c6c1bc6e4acd102772eb9d1edcbcabcc88939df8
youngbamer <0421mk@naver.com>
2021-10-21 13:39:20 +0900

    구조체 변수 활용 문제2

--- Project1/HelloWorld.c
+++ Project1/HelloWorld.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// 구조체 변수 활용 문제
+// 구조체 변수 활용 문제2
 struct employee {
 	char name[10];
 	char personNumber[20];
@@ -9,15 +9,22 @@ struct employee {
 
 int main() {
 
-	struct employee em;
+	struct employee em[3];
 
-	scanf_s("%s", em.name, sizeof(em.name));
-	scanf_s("%s", em.personNumber, sizeof(em.personNumber));
-	scanf_s("%d", &(em.payment));
+	for(int i = 0; i < 3; i++) {
+		printf("이름 : ");
+		scanf_s("%s", em[i].name, sizeof(em[i].name));
+		printf("주민등록번호 : ");
+		scanf_s("%s", em[i].personNumber, sizeof(em[i].personNumber));
+		printf("급여 : ");
+		scanf_s("%d", &(em[i].payment));
+	}
 
-	printf("이름 : %s\n", em.name);
-	printf("주민등록번호 : %s\n", em.personNumber);
-	printf("급여 : %d\n", em.payment);
+	for (int i = 0; i < 3; i++) {
+		printf("이름 : %s\n", em[i].name);
+		printf("주민등록번호 : %s\n", em[i].personNumber);
+		printf("급여 : %d\n", em[i].payment);
+	}
 
 	return 0;
 }
