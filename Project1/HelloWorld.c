d0c6e219bba9dd4242c2aaaefa29f2420ca42cb9
youngbamer <0421mk@naver.com>
2021-10-21 12:59:13 +0900

    사용자의 이름과 나이를 입력받아 비교하는 프로그램 구현

--- Project1/HelloWorld.c
+++ Project1/HelloWorld.c
@@ -1,24 +1,71 @@
 #include <stdio.h>
 #include <string.h>
 
-// strcpy, strcat 함수의 활용
+// 공백의 위치 return
+int getIndexByStr(char* str) {
+	int index = 0;
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] == ' ') {
+			index = i;
+		}
+	}
+
+	return index;
+}
+
+// 이름 비교
+int compareName(char* str1, char* str2, int index) {
+	int result = 0;
+	if (!strncmp(str1, str2, index)) {
+		result = 1;
+	}
+
+	return result;
+}
+
+// 나이 비교
+int compareAge(char* str1, char* str2) {
+	int result = 0;
+	int index1 = getIndexByStr(str1);
+	int index2 = getIndexByStr(str2);
+
+	int age1 = atoi(&str1[index1 + 1]);
+	int age2 = atoi(&str2[index2 + 1]);
+
+	if (age1 == age2) {
+		result = 1;
+	}
+
+	return result;
+}
+// 사용자의 이름과 나이를 입력받아 비교 작업
 int main() {
 	char str1[20];
 	char str2[20];
-	char str3[40];
 
-	fputs("str1 입력 : ", stdout);
+	fputs("첫번째 사용자를 입력해주세요.", stdout);
 	fgets(str1, sizeof(str1), stdin);
 	str1[strlen(str1) - 1] = 0;
+	int index1 = getIndexByStr(str1);
 
-	fputs("str2 입력 : ", stdout);
+	fputs("두번째 사용자를 입력해주세요.", stdout);
 	fgets(str2, sizeof(str2), stdin);
 	str2[strlen(str2) - 1] = 0;
+	int index2 = getIndexByStr(str2);
 
-	strcpy_s(str3, sizeof(str3), str1);
-	strcat_s(str3, sizeof(str3), str2);
-	
-	printf("str3 : %s", str3);
+	if (compareName(str1, str2, index1)) {
+		printf("이름이 같습니다.\n");
+	}
+	else {
+		printf("이름이 같지 않습니다.\n");
+	}
+
+	if (compareAge(str1, str2)) {
+		printf("나이가 동일합니다.\n");
+	}
+	else {
+		printf("나이가 동일하지 않습니다.\n");
+	}
 
 	return 0;
 }
