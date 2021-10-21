b811b8202c00f35ea5e189411bc49db4e1552a49
youngbamer <0421mk@naver.com>
2021-10-21 11:36:42 +0900

    getchar 함수와 putchar 함수의 활용

--- Project1/HelloWorld.c
+++ Project1/HelloWorld.c
@@ -1,56 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-// 난수를 이용해 strike, ball 게임 구현
+// getchar 함수와 putchar 함수의 활용
 int main() {
-	
-	srand((int)time(NULL));
-	
-	int com1 = rand() % 9 + 1;
-	int com2 = rand() % 9 + 1;
-	int com3 = rand() % 9 + 1;
+	int ch;
 
-	int com[3] = { com1, com2, com3 };
+	ch = getchar();
 
-	int input1, input2, input3;
-
-	int i = 1;
-	int game = 1;
-	while (game > 0) {
-		int strike = 0;
-		int ball = 0;
-
-		printf("3개의 숫자 선택 : ");
-		scanf_s("%d", &input1);
-		scanf_s("%d", &input2);
-		scanf_s("%d", &input3);
-
-		int input[3] = { input1, input2, input3 };
-
-		for (int j = 0; j < 3; j++) {
-			for (int i = 0; i < 3; i++) {
-				if (j == i) {
-					if (com[i] == input[j]) {
-						strike++;
-					}
-				}
-				else {
-					if (com[i] == input[j]) {
-						ball++;
-					}
-				}
-			}
+	if (ch >= 65 && ch <= 122) {
+		if (ch >= 65 && ch < 97) {
+			ch += 32;
 		}
-
-		if (strike == 3) {
-			game = -1;
+		else {
+			ch -= 32;
 		}
-
-		printf("%d번째 도전 결과: %dstrike, %dball!!\n", i, strike, ball);
+	}
+	else {
+		printf("알파벳만 입력해주세요.\n");
 	}
 	
-	
+	putchar(ch);
 
+	// A 65 a 97
 	return 0;
 }
