d917e07a372a8ef0283e83f426f4d36b997f311e
youngbamer <0421mk@naver.com>
2021-10-23 23:25:27 +0900

    파일 위치 지시자 예제 구현

--- Project1/HelloWorld.c
+++ Project1/HelloWorld.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 
-// 파일 출력 구현하기 Debug 폴더에 mystory.txt 파일
+// 파일 위치 지시자 예제
 int main() {
 	FILE* fp;
-	fopen_s(&fp, "mystory.txt", "wt");
-	
-	fputs("#이름: 최윤우 \n", fp);
-	fputs("#주민번호: 960421-1495888 \n", fp);
-	fputs("#전화번호: 010-4446-5052 \n", fp);
-
+	fopen_s(&fp, "text.txt", "wt");
+	fputs("123456789", fp);
 	fclose(fp);
 
+	fopen_s(&fp, "text.txt", "rt");
+	
+	/* SEEK_END test */
+	fseek(fp, -2, SEEK_END);
+	putchar(fgetc(fp));
+
+	/* SEEK_SET test */
+	fseek(fp, 2, SEEK_SET);
+	putchar(fgetc(fp));
+
+	/* SEEK_CUR test */
+	fseek(fp, 2, SEEK_CUR);
+	putchar(fgetc(fp));
+	
+	fclose(fp);
 	return 0;
 }
