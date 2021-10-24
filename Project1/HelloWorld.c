514c3e9901de4942e571ed2e0674e89dda7230c2
youngbamer <0421mk@naver.com>
2021-10-24 14:06:47 +0900

    선행처리기 예제 문제

--- Project1/HelloWorld.c
+++ Project1/HelloWorld.c
@@ -1,31 +1,16 @@
 #include <stdio.h>
+#define ADD(x, y, z) ((x)+(y)+(z))
+#define MUL(x, y, z) ((x)*(y)*(z))
+#define PI 3.141592
+#define AREA(r) ((r)*(r)*PI)
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
-// 정수를 입력받아 배열 출력, 사이즈는 realloc으로 수정 가능
+// 선행처리기 예제 문제
 int main() {
-	int* ptr = (int*) malloc(sizeof(int) * 5);
-	int size = 0;
+	printf("3+4+5 = %d\n", ADD(3, 4, 5));
+	printf("3*4*5 = %d\n", MUL(3, 4, 5));
+	printf("반지름 4인 원의 넓이 = %f\n", AREA(4));
+	printf("3, 4 중 더 큰 수는 : %d\n", MAX(3, 4));
 
-	while (1) {
-		scanf_s("%d", &ptr[size], sizeof(ptr));
-
-		if (ptr[size] == -1) {
-			printf("프로그램을 종료합니다.\n");
-			break;
-		}
-
-		size++;
-
-		if (_msize(ptr) <= sizeof(int) * size) {
-			(int*)realloc(ptr, _msize(ptr) + sizeof(int)*3);
-			printf("_msize(ptr) : %d\n", _msize(ptr));
-		}
-	}
-
-	for (int i = 0; i < size; i++) {
-		printf("%d ", ptr[i]);
-	}
-
-	free(ptr);
-	
 	return 0;
 }
