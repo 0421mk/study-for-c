70e715f608b1d1ac7d473b79470cc3d302ed020a
youngbamer <0421mk@naver.com>
2021-10-25 13:44:14 +0900

    fscanf 함수를 이용해 A와 P로 시작하는 단어의 수 구현

--- Project1/HelloWorld.c
+++ Project1/HelloWorld.c
@@ -1,46 +1,32 @@
 #include <stdio.h>
 
-typedef struct number {
-	double c;
-	double i;
-} Number;
-
-Number add(double c1, double i1, double c2, double i2);
-Number mul(double c1, double i1, double c2, double i2);
+// fscanf 함수를 이용해 A와 P로 시작하는 단어의 수 구현
 
 int main() {
-	Number num1;
-	Number num2;
-	
-	scanf_s("%lf %lf", &num1.c, &num1.i);
-	scanf_s("%lf %lf", &num2.c, &num2.i);
+	FILE* fp;
+	fopen_s(&fp, "wordcnt.txt", "rt");
 
-	Number result1;
-	Number result2;
+	char str[10];
+	int Acnt = 0;
+	int Pcnt = 0;
 
-	result1 = add(num1.c, num1.i, num2.c, num2.i);
-	result2 = mul(num1.c, num1.i, num2.c, num2.i);
+	while (feof(fp) != 1) {
+		fscanf_s(fp, "%s", str, sizeof(str));
 
-	printf("합의 결과] 실수: %lf, 허수: %lf\n", result1.c, result1.i);
-	printf("곱의 결과] 실수: %lf, 허수: %lf\n", result2.c, result2.i);
+		if (str[0] == 'A') {
+			Acnt++;
+		}
+		else if (str[0] == 'P') {
+			Pcnt++;
+		}
+
+		printf("%s, 위치 : %d, 파일의 끝 : %d\n", str, ftell(fp), feof(fp));
+	}
+
+	printf("A로 시작하는 단어의 수: %d\n", Acnt);
+	printf("P로 시작하는 단어의 수: %d\n", Pcnt);
+
+	fclose(fp);
 
 	return 0;
-}
-
-Number add(double c1, double i1, double c2, double i2) {
-	Number result;
-
-	result.c = c1 + c2;
-	result.i = i1 + i2;
-
-	return result;
-}
-
-Number mul(double c1, double i1, double c2, double i2) {
-	Number result;
-
-	result.c = (c1*c2) - (i1*i2);
-	result.i = (i1 * c2) + (c2 * i1);
-
-	return result;
 }
