d0219b05a60dee9525e3532cc2cf94b5a5166668
youngbamer <0421mk@naver.com>
2021-10-25 12:44:11 +0900

    구조체 포인터 배열, 동적 할당으로 구현

--- Project1/HelloWorld.c
+++ Project1/HelloWorld.c
@@ -6,25 +6,25 @@ typedef struct information {
 	int page;
 } Info;
 
-// 구조체 배열로 정보 입력받고 출력하기
-// 개행문자 입력받는것이 포인트
+// 구조체 포인터 배열
+// 동적할당으로 배열을 만들어도 [i]로 접근 가능
 
 int main() {
-	Info arr[3];
-	int number;
+	Info *info;
+	info = (Info*)malloc(sizeof(Info)*3);
 	
 	printf("도서 정보 입력\n");
 	for (int i = 0; i < 3; i++) {
 		printf("저자: ");
-		fgets(arr[i].name, sizeof(arr[i].name), stdin);
-		arr[i].name[strlen(arr[i].name) - 1] = 0;
+		fgets(info[i].name, sizeof(info[i].name), stdin);
+		info[i].name[strlen(info[i].name) - 1] = 0;
 
 		printf("제목: ");
-		fgets(arr[i].title, sizeof(arr[i].title), stdin);
-		arr[i].title[strlen(arr[i].title) - 1] = 0;
+		fgets(info[i].title, sizeof(info[i].title), stdin);
+		info[i].title[strlen(info[i].title) - 1] = 0;
 
 		printf("페이지 수: ");
-		scanf_s("%d", &arr[i].page, sizeof(arr[i].page));
+		scanf_s("%d", &info[i].page, sizeof(info[i].page));
 
 		// getchar()가 \n이면 반복문 break; 즉 getchar()을 읽고 break 시킴
 		while (getchar() != '\n');
@@ -34,10 +34,12 @@ int main() {
 	
 	for (int i = 0; i < 3; i++) {
 		printf("book %d\n", i + 1);
-		printf("저자 : %s\n", arr[i].name);
-		printf("제목 : %s\n", arr[i].title);
-		printf("페이지 수 : %d\n", arr[i].page);
+		printf("저자 : %s\n", info[i].name);
+		printf("제목 : %s\n", info[i].title);
+		printf("페이지 수 : %d\n", info[i].page);
 	}
+
+	free(info);
 
 	return 0;
 }
